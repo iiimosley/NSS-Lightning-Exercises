a547de517e469f7bcb761236495ba38f9ed62156
Trey Mosley <iiimosley@Treys-MacBook-Pro.local>
2017-12-04 09:22:25 -0600

    proper concat

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 const workers = [
-            { name: 'Bob', department: 'sales', title: 'sales manager'},
-            { name: 'Tina', department: 'finance', title: 'director of finance'},
-            { name: 'Randy', department: 'IT', title: 'hardware guy'},
-            { name: 'Glenda', department: 'C-suite', title: 'CEO'}
-        ];
+    { name: 'Bob', department: 'sales', title: 'sales manager' },
+    { name: 'Tina', department: 'finance', title: 'director of finance' },
+    { name: 'Randy', department: 'IT', title: 'hardware guy' },
+    { name: 'Glenda', department: 'C-suite', title: 'CEO' }
+];
 
-for (i=0; i<=workers.length; i++) {
-    console.log("Name:", workers[i].name + ",", "Job Title:", workers[i].title);
+for (i = 0; i <= workers.length; i++) {
+    console.log(workers[i].name + "'s job title is " + workers[i].title);
 }
