4584bfeeeae2d81a97d3044c178fb5e59a228494
Trey Mosley <iiimosley@Treys-MacBook-Pro.local>
2018-01-08 10:03:00 -0600

    jan8

--- README.md
+++ README.md
@@ -9,3 +9,5 @@
 * Jan2.js - array maniuplation, dot notation review
 
 * Jan4.js - loop and add numbers, incrementally
+
+* Jan8.js - object manipulation
---
+++ js/jan8.js
@@ -0,0 +1,48 @@
+// lightning exercise:
+// Loop through this object and add each member object's key as an ID. Log the results.
+
+let customers = {
+    active: {
+        a87b6345: {
+            name: "Gary Harrison",
+            age: 23,
+            member_level: "gold"
+        },
+        b345hy66: {
+            name: "Harry Garrison",
+            age: 34,
+            member_level: "silver"
+        },
+        nh56yht8: {
+            name: "Mary Terryson",
+            age: 78,
+            member_level: "platinum"
+        },
+        j677yd456: {
+            name: "Barry Sonnyman",
+            age: 44,
+            member_level: "gold"
+        }
+    },
+    inactive: {
+        ur56tfgt: {
+            name: "Barb Justice",
+            age: 56,
+            member_level: "none"
+        },
+        d56gt6: {
+            name: "Jane L'eggo",
+            age: 11,
+            member_level: "none"
+        }
+    }
+}
+
+Object.keys(customers).forEach(status=>{
+    let customerStatus = customers[status];
+    Object.keys(customerStatus).forEach(customer => {
+        customerStatus[customer].id = customer;
+    });
+})
+
+console.log(customers);
